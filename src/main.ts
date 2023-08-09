2d0f7638a49215b7886f39e69d4f239b9a6fd740
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-08-09 22:05:59 +0600

    🚧 feat: start creating register action

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
+import { provideStore } from '@ngrx/store'
 
 import { AppComponent } from 'src/app/app.component'
 import { appRoutes } from 'src/app/app.routes'
 
-bootstrapApplication(AppComponent, { providers: [provideRouter(appRoutes)] })
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes), provideStore()],
+})
