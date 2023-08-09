23dd201b3221277ee621e0ea1307e8e12a793f1e
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-08-09 22:24:40 +0600

    ✨ feat: implement register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Store } from '@ngrx/store'
+
+import { register } from 'src/app/auth/store/actions'
+import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
 
 @Component({
   selector: 'mc-register',
@@ -14,9 +18,15 @@ export class RegisterComponent {
     password: ['', Validators.required],
   })
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+  ) {}
 
   onSubmit(): void {
-    console.log(this.form.value)
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    }
+    this.store.dispatch(register({ request }))
   }
 }
--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,3 +1,7 @@
-import { createAction } from '@ngrx/store'
+import { createAction, props } from '@ngrx/store'
+import { RegisterRequestInterface } from '../types/registerRequest.interface'
 
-export const register = createAction('[Auth] Register')
+export const register = createAction(
+  '[Auth] Register',
+  props<{ request: RegisterRequestInterface }>(),
+)
