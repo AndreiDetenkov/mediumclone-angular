b98dfacc19d73bdde0b9c64329de956d6df0608c
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-08-09 22:38:55 +0600

    ➕ chore: install and setup store-devtools

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
 import { provideStore } from '@ngrx/store'
+import { isDevMode } from '@angular/core'
+import { provideStoreDevtools } from '@ngrx/store-devtools'
 
 import { AppComponent } from 'src/app/app.component'
 import { appRoutes } from 'src/app/app.routes'
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideStore()],
+  providers: [
+    provideRouter(appRoutes),
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
 })
