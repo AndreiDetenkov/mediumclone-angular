580352cfaed6acc9fc61b3427b40b7c0467e3379
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-12-03 12:36:43 +0600

    ♻️ refactor: update clone fn argument

--- src/app/shared/services/authInterceptor.ts
+++ src/app/shared/services/authInterceptor.ts
@@ -1,15 +1,18 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http'
 import { inject } from '@angular/core'
+
 import { PersistenceService } from './persistence.service'
 
 export const authInterceptor: HttpInterceptorFn = (
   request: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
-  const persistenceService = inject(PersistenceService)
+  const persistenceService: PersistenceService = inject(PersistenceService)
   const token = persistenceService.get('accessToken')
   const modifiedReq = request.clone({
-    headers: request.headers.set('Authorization', token ? `Token ${token}` : ''),
+    setHeaders: {
+      Authorization: token ? `Token ${token}` : '',
+    },
   })
   return next(modifiedReq)
 }
