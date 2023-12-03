cd57ea437797505e5fd6a6012c9a01aa7a597282
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-12-03 12:18:09 +0600

    ✨ feat: implement Auth interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
 import { provideState, provideStore } from '@ngrx/store'
 import { isDevMode } from '@angular/core'
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideStoreDevtools } from '@ngrx/store-devtools'
 import { provideEffects } from '@ngrx/effects'
 import { provideRouterStore, routerReducer } from '@ngrx/router-store'
@@ -11,10 +11,11 @@ import { AppComponent } from 'src/app/app.component'
 import { appRoutes } from 'src/app/app.routes'
 import { authFeature } from 'src/app/auth/store/reducers'
 import * as authEffects from 'src/app/auth/store/effects'
+import { authInterceptor } from './app/shared/services/authInterceptor'
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(appRoutes),
     provideStore({ router: routerReducer }),
     provideRouterStore(),
