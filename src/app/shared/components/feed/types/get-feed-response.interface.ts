d33ccfb955a28ee3b8652bc1f034084b187fd785
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-12-04 10:28:56 +0600

    ✨ feat: create feed service

---
+++ src/app/shared/components/feed/types/get-feed-response.interface.ts
@@ -0,0 +1,6 @@
+import { ArticlesInterface } from '../../../types/articles.interface'
+
+export interface GetFeedResponseInterface {
+  articles: ArticlesInterface
+  articlesCount: number
+}
---
+++ src/app/shared/types/articles.interface.ts
@@ -0,0 +1,11 @@
+export interface ArticlesInterface {
+  body: string
+  createdAt: string
+  description: string
+  favorited: boolean
+  favoritesCount: number
+  slug: string
+  tagList: string[]
+  title: string
+  updatedAt: string
+}
