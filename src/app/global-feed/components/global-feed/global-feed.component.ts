7e7a7d614b2ad1bc6516d1a1a78d4c0d537dfe02
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-12-07 11:49:03 +0600

    ✨ feat: add banner component

--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { FeedComponent } from '../../../shared/components/feed/feed.component'
+import { BannerComponent } from '../../../shared/components/banner/banner.component'
 
 @Component({
   selector: 'mc-global-feed',
   templateUrl: './global-feed.component.html',
   standalone: true,
-  imports: [CommonModule, FeedComponent],
+  imports: [CommonModule, FeedComponent, BannerComponent],
 })
 export class GlobalFeedComponent {
   apiUrl = '/articles'
